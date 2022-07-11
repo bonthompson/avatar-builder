9e796bc2829154a8f7e8d4802bf85cc43e6081e5
lailacharaoui <mats2@live.fr>
2022-07-11 15:01:59 +0200

    feat(config): add base items config

---
+++ src/config/items.js
@@ -0,0 +1,18 @@
+const ITEMS = {
+    TOPS : {
+        T_SHIRT : 't-shirt',
+    },
+    EYES: {
+        EYES_1: 'eyes-1',
+        EYES_2: 'eyes-2',
+        EYES_3: 'eyes-3',
+    },
+    HAIR: {
+        LONG_HAIR: 'long-hair',
+    },
+    MOUTH: {
+        MOUTH_1: 'mouth-1',
+        MOUTH_2: 'mouth-2',
+    }
+
+}
